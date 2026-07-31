43e202bbdb14fdb0a8dd0568cf4a821ffa89b200
Shubhamtem2006 <shubhamtembhre47@gmail.com>
2026-07-31 16:00:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,76 @@
 let text = "Web Design";
 let i = 0;
 
-function type(){
-  if(i < text.length){
-    document.getElementById("typing").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(type,150);
-  }
+function type() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, 150);
+    }
 }
 type();
 
-function toggleMenu(){
-    document.getElementById("menuBox").classList.toggle("active");
-}
-
-document.querySelectorAll("#menuBox a").forEach(function(link){
-    link.addEventListener("click", function(){
-        document.getElementById("menuBox").classList.remove("active");
-    });
-});
-function toggleTheme(){
+function toggleTheme() {
 
     document.body.classList.toggle("dark");
 
     let btn = document.getElementById("themeBtn");
 
-    if(document.body.classList.contains("dark")){
-        btn.innerHTML="☀️";
-    }else{
-        btn.innerHTML="🌙";
+    if (document.body.classList.contains("dark")) {
+        btn.innerHTML = "☀️";
+    } else {
+        btn.innerHTML = "🌙";
     }
 
 }
+// document.addEventListener("click", function(event){
+
+//     let menu = document.getElementById("menuBox");
+//     let menuBtn = document.querySelector(".menu");
+
+//     if(!menu.contains(event.target) && !menuBtn.contains(event.target)){
+//         menu.classList.remove("active");
+//     }
+
+// });
+
+function toggleMenu(event) {
+    event.stopPropagation();
+    document.getElementById("menuBox").classList.toggle("active");
+}
+
+document.getElementById("menuBox").addEventListener("click", function (e) {
+    e.stopPropagation();
+});
+
+document.addEventListener("click", function () {
+    document.getElementById("menuBox").classList.remove("active");
+});
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        name: form.name.value,
+        email: form.email.value,
+        phone: form.phone.value,
+        message: form.message.value
+    };
+
+    const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+   if(result.success) {window.location.href = "thanks.html";}
+    else{alert("Registration Failed");
+        
+    }
+});
